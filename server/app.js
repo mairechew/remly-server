67b858543d5d3cb2d562b1dbcc008549592bdf2d
Maire Chew <mairechew@gmail.com>
2017-12-06 16:38:00 -0700

    working

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var cors = require('cors');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
@@ -15,6 +16,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 
 app.use('/api/v1/urls', urls);
 
