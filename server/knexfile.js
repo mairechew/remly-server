91a264523704575c9c452506572dcb55e6400a45
Maire Chew <mairechew@gmail.com>
2017-12-06 15:35:01 -0700

    working

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,9 +1,12 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
     client: 'pg',
     connection: 'postgres://localhost/ar-images'
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + '?ssl=true'
   }
 };
